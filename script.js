338ac6467069ad4c4f3923bd38059be2fdfa052a
sunny2831 <sunny2831@hotmail.co.uk>
2020-12-10 17:07:30 +0000

    added open/close modal feature, and code refactored

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const closeModalBtn = document.querySelector('.close-modal');
 const showModalBtns = document.querySelectorAll('.show-modal');
-console.log(showModalBtns);
 
-for (let i = 0; i < showModalBtns.length; i++)
-  console.log(showModalBtns[i].textContent);
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < showModalBtns.length; i++) {
+  showModalBtns[i].addEventListener('click', openModal);
+}
+
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
