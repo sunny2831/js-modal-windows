78e8684fcd2a35e3d162b72450a00e8b7215bf5c
sunny2831 <sunny2831@hotmail.co.uk>
2020-12-10 15:00:39 +0000

    selected + stored  DOM elements in JS variable

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.close-modal');
+const showModalBtns = document.querySelectorAll('.show-modal');
+console.log(showModalBtns);
+
+for (let i = 0; i < showModalBtns.length; i++)
+  console.log(showModalBtns[i].textContent);
